20684a34d79af90af699cccbd77b9fb50b9c9df6
DZ-tt2195 <tt2195@nyu.edu>
2025-01-13 18:23:12 -0800

    recover lost enemy

---
+++ Assets/Scripts/Specific/MoveUpDown.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class MoveUpDown : BaseEnemy
+{
+    protected override void Awake()
+    {
+        base.Awake();
+        moveDirection = Vector3.down;
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+        if (this.transform.position.y < WaveManager.minY)
+            moveDirection = Vector3.up;
+        else if (transform.position.y > WaveManager.maxY)
+            moveDirection = Vector3.down;
+    }
+
+    protected override void ShootBullet()
+    {
+        CreateBullet(prefab, this.transform.position, bulletSpeed, new(-1, -1));
+        CreateBullet(prefab, this.transform.position, bulletSpeed, new(-1, 0));
+        CreateBullet(prefab, this.transform.position, bulletSpeed, new(-1, 1));
+
+        CreateBullet(prefab, this.transform.position, bulletSpeed, new(1, -1));
+        CreateBullet(prefab, this.transform.position, bulletSpeed, new(1, 0));
+        CreateBullet(prefab, this.transform.position, bulletSpeed, new(1, 1));
+    }
+}
